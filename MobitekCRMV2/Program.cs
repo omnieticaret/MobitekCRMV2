d8a58103d34cac6014ab5f2ff5b88cdee0cac7fc
burakertan11 <burak.ertan@mobitek.com>
2024-12-19 16:40:04 +0300

    fix

--- MobitekCRMV2/Program.cs
+++ MobitekCRMV2/Program.cs
@@ -118,11 +118,12 @@ builder.Services.AddHttpContextAccessor();
 
 var app = builder.Build();
 
-if (app.Environment.IsDevelopment())
+app.UseSwagger();
+app.UseSwaggerUI(c =>
 {
-    app.UseSwagger();
-    app.UseSwaggerUI();
-}
+    c.SwaggerEndpoint("/swagger/v1/swagger.json", "Mobitek CRM API v1");
+    c.RoutePrefix = "swagger";
+});
 
 app.UseCors("AllowLocalhost3000");
 
